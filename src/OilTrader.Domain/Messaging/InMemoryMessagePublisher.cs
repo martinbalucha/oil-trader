c3ca0561a2a9fd0a64e3a5afc4b04dfa612432a5
martinbalucha <martin.balucha@outlook.com>
2026-05-18 12:56:10 +0200

    Draft message publisher
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>

---
+++ src/OilTrader.Domain/Messaging/InMemoryMessagePublisher.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+using Microsoft.Extensions.Logging;
+using OilTrader.Contracts.Messaging;
+
+namespace OilTrader.Domain.Messaging;
+
+/// <summary>
+/// In-memory version of the message publisher.
+/// </summary>
+public class InMemoryMessagePublisher : IMessagePublisher
+{
+    private readonly ILogger<InMemoryMessagePublisher> _logger;
+    private readonly Channel<IMessage> _channel = Channel.CreateUnbounded<IMessage>();
+
+    public InMemoryMessagePublisher(ILogger<InMemoryMessagePublisher> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task PublishAsync<T>(T message, CancellationToken ct = default) where T : class, IMessage
+    {
+        await _channel.Writer.WriteAsync(message, ct);
+
+        _logger.LogInformation("Successfully published {Type} message with id='{MessageId}'",
+            message.GetType().Name, message.Id);
+    }
+}
