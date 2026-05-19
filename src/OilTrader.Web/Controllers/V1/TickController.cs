2195fc1e7b12dc308571e18982851e2c1db0428d
martinbalucha <38705514+martinbalucha@users.noreply.github.com>
2026-05-19 11:19:16 +0200

    12 api implement tickcontroller post tick (#21)
    * Craete tick controller
    
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>
    
    * Add swagger and exception handler
    
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>
    
    * Ran dotnet format
    
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>
    
    * Update doc
    
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>
    
    ---------
    
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>

---
+++ src/OilTrader.Web/Controllers/V1/TickController.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using OilTrader.Contracts.TickManagement;
+using OilTrader.Web.Models;
+
+namespace OilTrader.Web.Controllers.V1;
+
+/// <summary>
+/// API Controller for Tick operations
+/// </summary>
+[ApiController]
+[ApiVersion(1)]
+[Route("api/v{version:apiVersion}/tick")]
+public class TickController : ControllerBase
+{
+    private readonly ITickService _tickService;
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    /// <param name="tickService">Tick service</param>
+    public TickController(ITickService tickService)
+    {
+        _tickService = tickService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status204NoContent, Description = "The tick is processed successfully")]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest, Description = "Tick is invalid")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError, Description = "Internal server error")]
+    public async Task<IActionResult> PostTickAsync([FromBody] TickRequest request, CancellationToken ct)
+    {
+        var tick = request.ToDomain();
+
+        await _tickService.ProcessAsync(tick, ct);
+
+        return NoContent();
+    }
+}
