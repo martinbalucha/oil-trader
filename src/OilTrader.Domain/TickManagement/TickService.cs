66b122f1d3671208d11dc8cff1a81b8c7f41f34b
martinbalucha <martin.balucha@outlook.com>
2026-05-19 07:43:38 +0200

    Drafted up tick service
    Signed-off-by: martinbalucha <martin.balucha@outlook.com>

---
+++ src/OilTrader.Domain/TickManagement/TickService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using OilTrader.Contracts;
+using OilTrader.Contracts.TickManagement;
+
+namespace OilTrader.Domain.TickManagement;
+
+public class TickService : ITickService
+{
+    private readonly ITimeframeAggregator _timeframeAggregator;
+    private readonly ITickRepository _repository;
+    private readonly ILogger<TickService> _logger;
+
+    public TickService(ITimeframeAggregator timeframeAggregator, ITickRepository repository, ILogger<TickService> logger)
+    {
+        _timeframeAggregator = timeframeAggregator;
+        _repository = repository;
+        _logger = logger;
+    }
+
+    public Task ProcessAsync(Tick tick, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
